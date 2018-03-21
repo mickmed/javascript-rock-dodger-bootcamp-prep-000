703d87618424569235a974eb3417419feaf80bb2
Michael Roth <mick2090@gmail.com>
2018-03-21 17:14:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -132,9 +132,8 @@ function moveDodger(e) {
 
 function moveDodgerLeft() {
   var leftNumbers = dodger.style.left.replace('px', '')
-  
- 
   var left = parseInt(leftNumbers, 10)
+  
  
   if (left > 0) {
     dodger.style.left = `${left - 1}px`
